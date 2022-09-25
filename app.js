9065780012c3172cdd4c3f8a9d8af258c4dd9e28
Udosinachi <62754095+udosinachi@users.noreply.github.com>
2022-09-25 10:23:35 +0100

    Updating stories

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const { engine } = require("express-handlebars");
+const methodOverride = require("method-override");
 const passport = require("passport");
 const mongoose = require("mongoose");
 const session = require("express-session");
@@ -21,6 +22,18 @@ const app = express();
 // Body Parser
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Method Override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
 
 //logging
 if (process.env.NODE_ENV === "development") {
--- routes/stories.js
+++ routes/stories.js
@@ -61,4 +61,25 @@ router.get("/edit/:id", ensureAuth, async (req, res) => {
   }
 });
 
+// Update Stories
+// PUT /stories/:id
+router.put("/:id", ensureAuth, async (req, res) => {
+  let story = await Story.findById(req.params.id).lean();
+
+  if (!story) {
+    return res.render("/stories");
+  }
+
+  if (story.user != req.user.id) {
+    res.redirect("/stories");
+  } else {
+    story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.redirect("/dashboard");
+  }
+});
+
 module.exports = router;
