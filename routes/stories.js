44065ad51e97b93365d1f4c1e4afa795b72497e9
Udosinachi <62754095+udosinachi@users.noreply.github.com>
2022-09-27 05:01:30 +0100

    Viewing individual user  stories

--- routes/stories.js
+++ routes/stories.js
@@ -4,14 +4,14 @@ const { ensureAuth } = require("../middleware/auth");
 
 const Story = require("../models/Story");
 
-// Show add page
-// GET /stories/add
+//    Show add page
+//    GET /stories/add
 router.get("/add", ensureAuth, (req, res) => {
   res.render("stories/add");
 });
 
-// Process add form
-// POST /stories
+//    Process add form
+//    POST /stories
 router.post("/", ensureAuth, async (req, res) => {
   try {
     req.body.user = req.user.id;
@@ -23,8 +23,8 @@ router.post("/", ensureAuth, async (req, res) => {
   }
 });
 
-// Show all stories
-// GET /stories
+//    Show all stories
+//    GET /stories
 router.get("/", ensureAuth, async (req, res) => {
   try {
     const stories = await Story.find({ status: "public" })
@@ -41,44 +41,119 @@ router.get("/", ensureAuth, async (req, res) => {
   }
 });
 
-// Show edit page
-// GET /stories/edit/:id
-router.get("/edit/:id", ensureAuth, async (req, res) => {
-  const story = await Story.findOne({
-    _id: req.params.id,
-  }).lean();
+//    Show single story
+//    GET /stories/:id
+router.get("/:id", ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).populate("user").lean();
 
-  if (!story) {
-    return res.render("error/404");
-  }
+    if (!story) {
+      return res.render("error/404");
+    }
 
-  if (story.user != req.user.id) {
-    res.redirect("/stories");
-  } else {
-    res.render("stories/edit", {
-      story,
-    });
+    if (story.user._id != req.user.id && story.status == "private") {
+      res.render("error/404");
+    } else {
+      res.render("stories/show", {
+        story,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    res.render("error/404");
   }
 });
 
-// Update Stories
-// PUT /stories/:id
-router.put("/:id", ensureAuth, async (req, res) => {
-  let story = await Story.findById(req.params.id).lean();
+//    Show edit page
+//    GET /stories/edit/:id
+router.get("/edit/:id", ensureAuth, async (req, res) => {
+  try {
+    const story = await Story.findOne({
+      _id: req.params.id,
+    }).lean();
 
-  if (!story) {
-    return res.render("/stories");
+    if (!story) {
+      return res.render("error/404");
+    }
+
+    if (story.user != req.user.id) {
+      res.redirect("/stories");
+    } else {
+      res.render("stories/edit", {
+        story,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    return res.render("error/500");
   }
+});
 
-  if (story.user != req.user.id) {
-    res.redirect("/stories");
-  } else {
-    story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
-      new: true,
-      runValidators: true,
+//    Update story
+//    PUT /stories/:id
+router.put("/:id", ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).lean();
+
+    if (!story) {
+      return res.render("error/404");
+    }
+
+    if (story.user != req.user.id) {
+      res.redirect("/stories");
+    } else {
+      story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true,
+      });
+
+      res.redirect("/dashboard");
+    }
+  } catch (err) {
+    console.error(err);
+    return res.render("error/500");
+  }
+});
+
+//    Delete story
+//    DELETE /stories/:id
+router.delete("/:id", ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).lean();
+
+    if (!story) {
+      return res.render("error/404");
+    }
+
+    if (story.user != req.user.id) {
+      res.redirect("/stories");
+    } else {
+      await Story.remove({ _id: req.params.id });
+      res.redirect("/dashboard");
+    }
+  } catch (err) {
+    console.error(err);
+    return res.render("error/500");
+  }
+});
+
+//    User stories
+//    GET /stories/user/:userId
+router.get("/user/:userId", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({
+      user: req.params.userId,
+      status: "public",
+    })
+      .populate("user")
+      .lean();
+
+    res.render("stories/index", {
+      stories,
     });
-
-    res.redirect("/dashboard");
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
   }
 });
 
